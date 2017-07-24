a32232aeb8613fd1507d0273422efb97768453ff
colmexdev <github@colmex.mx>
2017-07-24 18:28:17 -0500

    Primer commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/' => 'inicio#inicio', :as => :main
+  get 'inicio' => 'inicio#inicio', :as => :main
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
