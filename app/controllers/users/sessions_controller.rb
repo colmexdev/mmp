5fa9dde865b99b63b551f0a41911b5d18fd8bab9
colmexdev <github@colmex.mx>
2017-07-25 17:33:00 -0500

    Login usuarios

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,4 +1,17 @@
 class Users::SessionsController < Devise::SessionsController
+  def new
+    super
+  end
+
+  def create
+    super
+  end
+
+  def destroy
+    super
+  end
+
+
   protected
   def after_sign_in_path_for(resource)
     prueba_path
