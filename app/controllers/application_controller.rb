2c21baece04d55812cb5087f2fae06ba0c29cadc
colmexdev <github@colmex.mx>
2017-07-26 11:57:46 -0500

    Login usuarios

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,9 @@ class ApplicationController < ActionController::Base
 
   protected
   def after_sign_in_path_for(resource)
-    
+    logger.debug resource.inspect
+    logger.debug prueba_path
+    logger.debug params
     prueba_path
     #if resource_name == "user"
     #  prueba_path
@@ -14,7 +16,9 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(resource_or_scope)
     logger.debug resource_or_scope.inspect
-    root_path
+    if resource_or_scope == :user
+      root_path
+    end
   end
 
   def configure_permitted_parameters
