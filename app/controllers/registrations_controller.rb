0f17920cbb7aaa76fe36029685b8521a784d5c31
colmexdev <github@colmex.mx>
2017-07-27 13:31:32 -0500

    Login usuarios

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,15 +6,12 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def create
-    logger.debug sign_up_params
     build_resource(sign_up_params)
     respond_to do |format|
       if resource.save
-        logger.debug @user.errors
         format.html { redirect_to root_path, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
+        #format.json { render :show, status: :created, location: @user }
       else
-        logger.debug @user.errors
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
