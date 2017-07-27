d2a6605c88041ea71ac042d621560bb003bd27f2
colmexdev <github@colmex.mx>
2017-07-27 12:18:09 -0500

    Login usuarios

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   protected
   def after_sign_in_path_for(resource)
     logger.debug resource
@@ -10,6 +12,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_up_path_for(resource)
+    logger.debug resource
     root_path
   end
 
