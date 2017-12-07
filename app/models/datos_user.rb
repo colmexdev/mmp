95890085bedd214901abbf1cc12cfeeb93557929
colmexdev <github@colmex.mx>
2017-12-07 17:43:45 -0600

    Comentarios y fecha

--- app/models/datos_user.rb
+++ app/models/datos_user.rb
@@ -2,7 +2,7 @@ class DatosUser < ActiveRecord::Base
   validates_presence_of :nombre
 #  validates_presence_of :pref
   validates_presence_of :localidad
-#  validates_format_of :email, with: Devise::email_regexp
+  validates_format_of :email, with: Devise::email_regexp
 #  validates :telefono, presence: true, if: "email.blank?"
-#  validates :pref, acceptance: {accept: ['Correo electrónico','Llamada telefónica','Mensaje (Whatsapp)'], message: "no puede quedar vacío."}
+  validates :pref, acceptance: {accept: ['Correo electrónico','Llamada telefónica','Mensaje (Whatsapp)'], message: "no puede quedar vacío."}
 end
